3f51de8c2d16339d3c03a2e9f09ac456bf15d5ad
Max <silverhammermba@gmail.com>
2014-03-16 23:51:02 -0400

    First (naive) attempt
    Really obvious artifacts from sudden switch between opposite light maps

--- fragment.glsl
+++ fragment.glsl
@@ -14,7 +14,14 @@ uniform sampler2D bottom;
 
 void main()
 {
-	vec2 light = vec2(10.0 + sin(time / 1000.0) * 20.0, 28.0 + cos(time / 1000.0) * 30.0);
+	vec2 light = vec2(22.5 + sin(time / 2000.0) * 60.0, 28.0 + cos(time / 2000.0) * 60.0);
 
-	outColor = texture(bottom, TexCoord) * vec4(vec3((50.0 - clamp(distance(light, WorldPos), 0.0, 50.0)) / 50.0), 1.0);
+	vec2 light_dir = normalize(light - WorldPos);
+	float lightness =
+		clamp( light_dir.x, 0.0, 1.0) * texture(right,  TexCoord).x +
+		clamp(-light_dir.x, 0.0, 1.0) * texture(left,   TexCoord).x +
+		clamp( light_dir.y, 0.0, 1.0) * texture(top,    TexCoord).x +
+		clamp(-light_dir.y, 0.0, 1.0) * texture(bottom, TexCoord).x;
+
+	outColor = texture(ambient, TexCoord) * vec4(vec3(lightness), 1.0);
 }
