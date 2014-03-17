0dce2341eb7b63b2f9bcfea2316b64d73235c85e
Max <silverhammermba@gmail.com>
2014-03-17 00:04:19 -0400

    Second (naive) approach
    Assuming lights are purely determined by normals. Requires discarding
    half of the source images, looks terrible in 3/4 of quadrants

--- fragment.glsl
+++ fragment.glsl
@@ -17,11 +17,8 @@ void main()
 	vec2 light = vec2(22.5 + sin(time / 2000.0) * 60.0, 28.0 + cos(time / 2000.0) * 60.0);
 
 	vec2 light_dir = normalize(light - WorldPos);
-	float lightness =
-		clamp( light_dir.x, 0.0, 1.0) * texture(right,  TexCoord).x +
-		clamp(-light_dir.x, 0.0, 1.0) * texture(left,   TexCoord).x +
-		clamp( light_dir.y, 0.0, 1.0) * texture(top,    TexCoord).x +
-		clamp(-light_dir.y, 0.0, 1.0) * texture(bottom, TexCoord).x;
+	vec2 normal = normalize(vec2(-2.0 * texture(left, TexCoord).x + 1.0, 2.0 * texture(top, TexCoord).x - 1.0));
+	float lightness = clamp(dot(light_dir, normal), 0.0, 1.0);
 
 	outColor = texture(ambient, TexCoord) * vec4(vec3(lightness), 1.0);
 }
