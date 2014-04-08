5129a4a3df0c905760ff4c3521d5e05e4ce143ec
Max <silverhammermba@gmail.com>
2014-04-08 19:16:42 -0400

    Try weighted pyramid model
    Still obvious artifacts when transitioning between opposite maps

--- fragment.glsl
+++ fragment.glsl
@@ -14,15 +14,28 @@ uniform sampler2D bottom;
 
 void main()
 {
-	vec2 light = vec2(22.5 + sin(time / 500.0) * 42.5, 28.0 + cos(time / 500.0) * 48.0);
+	vec2 light = vec2(22.5 + sin(time / 5000.0) * 42.5, 28.0 + cos(time / 5000.0) * 48.0);
 
-	vec2 light_dir = light - floor(WorldPos) + 0.5;
-	light_dir = light_dir / (abs(light_dir.x) + abs(light_dir.y)); // make components sum to 1
-	float lightness =
-		clamp(light_dir.x, 0.0, 1.0) * texture(right, TexCoord).x +
-		clamp(light_dir.y, 0.0, 1.0) * texture(top, TexCoord).x +
-		clamp(-light_dir.x, 0.0, 1.0) * texture(left, TexCoord).x +
-		clamp(-light_dir.y, 0.0, 1.0) * texture(bottom, TexCoord).x;
+	vec2 light_dir = normalize(light - floor(WorldPos) + 0.5);
 
-	outColor = texture(ambient, TexCoord) * vec4(vec3(lightness), 1.0);
+	// store light info in a vector
+	vec4 c;
+	c.x = texture(right, TexCoord).x;
+	c.y = texture(top, TexCoord).x;
+	c.z = texture(left, TexCoord).x;
+	c.w = texture(bottom, TexCoord).x;
+	// use c/2 to restrict to [0.0, 0.5] and get a plausible projected area
+	vec4 a = (c / 2.0) / (c / -2.0 + 1.0);
+	// assume normal is a function of projected area
+	vec4 n = 1 / (a + 1.0);
+
+	// weighted sum, times 2 to make up for previous down scaling
+	float lightness = 2 * (
+		a.x * clamp(light_dir.x * n.x, 0.0, 1.0) +
+		a.y * clamp(light_dir.y * n.y, 0.0, 1.0) +
+		a.z * clamp(light_dir.x * -n.z, 0.0, 1.0) +
+		a.w * clamp(light_dir.y * -n.w, 0.0, 1.0)
+	);
+
+	outColor = texture(ambient, TexCoord).a * vec4(vec3(lightness), 1.0);
 }
